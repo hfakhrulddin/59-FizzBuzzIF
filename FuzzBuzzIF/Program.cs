4d079b3147bf0fef774cd9e13737e50e3e6f7ed6
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 21:56:08 -0700

    init2

---
+++ FuzzBuzzIF/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzBuzzIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+
+            {
+
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.Write("FizzBuzz" + "\r\n");
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz" + "\r\n");
+                    continue;
+                }
+
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz" + "\r\n");
+                    continue;
+                }
+
+                else
+
+                { Console.WriteLine(i); }
+
+            }
+
+            Console.Read();
+        }
+    }
+}
